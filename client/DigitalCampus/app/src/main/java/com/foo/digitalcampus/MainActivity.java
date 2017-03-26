c7bb01234f3bf5a3a5cd87123b0e0b4dc464b8d6
李锐 <ray007great@foxmail,com>
2017-03-26 23:30:57 +0800

    add viewpager and tab basic stuff

--- client/DigitalCampus/app/src/main/java/com/foo/digitalcampus/MainActivity.java
+++ client/DigitalCampus/app/src/main/java/com/foo/digitalcampus/MainActivity.java
@@ -1,13 +1,23 @@
 package com.foo.digitalcampus;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    TabLayout mainTabLayout;
+    ViewPager mainViewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainTabLayout = (TabLayout) findViewById(R.id.main_tab);
+        mainViewPager = (ViewPager) findViewById(R.id.main_viewpager);
+        mainViewPager.setAdapter(new MainFragmentPagerAdapter(getSupportFragmentManager(),
+                MainActivity.this));
+        mainTabLayout.setupWithViewPager(mainViewPager);
     }
 }
---
+++ client/DigitalCampus/app/src/main/java/com/foo/digitalcampus/MainFragmentPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.foo.digitalcampus;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by lirui on 2017/3/26.
+ */
+
+public class MainFragmentPagerAdapter extends FragmentPagerAdapter{
+    final int PAGE_COUNT = 3;
+    private String tabTitles[] = new String[] { "Tab1", "Tab2", "Tab3" };
+    private Context context;
+
+    public MainFragmentPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return PageFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+}

82ced449d605b486d69c23994c2c6a02dbb3895f
李锐 <ray007great@foxmail,com>
2017-03-26 11:36:51 +0800

    add main activity basic title and bottom

--- client/DigitalCampus/app/src/main/java/com/foo/digitalcampus/LoginActivity.java
+++ client/DigitalCampus/app/src/main/java/com/foo/digitalcampus/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.foo.digitalcampus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_login:
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+        }
     }
 }

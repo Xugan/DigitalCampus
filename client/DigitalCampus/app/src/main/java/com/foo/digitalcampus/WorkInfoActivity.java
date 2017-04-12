a3a636b402f5a7c04a2407752291a2f3d46318eb
Joker-Shen <454165967@qq.com>
2017-04-12 17:42:46 +0800

    get SystemTime

--- client/DigitalCampus/app/src/main/java/com/foo/digitalcampus/WorkInfoActivity.java
+++ client/DigitalCampus/app/src/main/java/com/foo/digitalcampus/WorkInfoActivity.java
@@ -2,10 +2,14 @@ package com.foo.digitalcampus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.lang.reflect.Array;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class WorkInfoActivity extends AppCompatActivity {
     private Spinner spDepartment;
@@ -14,16 +18,25 @@ public class WorkInfoActivity extends AppCompatActivity {
     private String[] status = {"驳回","处理中","处理完"};
     private ArrayAdapter<String> departmentAdapter;
     private ArrayAdapter<String> StatusAdapter;
+    private TextView tvSystemTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work_info);
         spDepartment = (Spinner) findViewById(R.id.spDepartment);
         spStatus = (Spinner) findViewById(R.id.spStatus);
+        tvSystemTime = (TextView) findViewById(R.id.tvSystemTime);
+
         departmentAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,departments);
         StatusAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,status);
-
         spDepartment.setAdapter(departmentAdapter);
         spStatus.setAdapter(StatusAdapter);
+
+        //获取系统时间
+        SimpleDateFormat format = new SimpleDateFormat("yyyy年MM月dd日   HH:mm:ss");
+        Date currentDate = new Date(System.currentTimeMillis());
+        String str = format.format(currentDate);
+        tvSystemTime.setText(str);
+        Log.i("SystemTime",str);
     }
 }
